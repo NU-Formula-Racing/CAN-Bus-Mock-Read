bb1e10fc6301a80e7c2e3032d34effedb269126d
izo0619 <isabelzhong30@gmail.com>
2022-11-16 21:56:39 -0600

    two bus read test

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 #if defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY41)
 #include "teensy_can.h"
-TeensyCAN<1> can_bus{};
+TeensyCAN<1> can_bus1{};
+TeensyCAN<2> can_bus2{};
 #endif
 
 #ifdef ARDUINO_ARCH_ESP32
@@ -27,10 +28,10 @@ CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40)> bl_brake_temperature;
 CANSignal<float, 16, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(-40)> br_brake_temperature;
 
-CANRXMessage<2> fl_wheel_msg{can_bus, 0x400, fl_wheel_speed, fl_brake_temperature};
-CANRXMessage<2> fr_wheel_msg{can_bus, 0x401, fr_wheel_speed, fr_brake_temperature};
-CANRXMessage<2> bl_wheel_msg{can_bus, 0x402, bl_wheel_speed, bl_brake_temperature};
-CANRXMessage<2> br_wheel_msg{can_bus, 0x403, br_wheel_speed, br_brake_temperature};
+CANRXMessage<2> fl_wheel_msg{can_bus1, 0x400, fl_wheel_speed, fl_brake_temperature};
+CANRXMessage<2> fr_wheel_msg{can_bus1, 0x401, fr_wheel_speed, fr_brake_temperature};
+CANRXMessage<2> bl_wheel_msg{can_bus2, 0x402, bl_wheel_speed, bl_brake_temperature};
+CANRXMessage<2> br_wheel_msg{can_bus2, 0x403, br_wheel_speed, br_brake_temperature};
 #endif
 
 #ifdef BRAKE_PRESSURE
@@ -42,7 +43,8 @@ CANRXMessage<1> back_brake_pressure_msg{can_bus, 0x411, rear_brake_pressure};
 
 void print()
 {
-  can_bus.Tick();
+  can_bus1.Tick();
+  can_bus2.Tick();
   float test_float = fl_wheel_speed;
   // rx on teensy
   Serial.print("Received fl_wheel_speed: ");
@@ -69,38 +71,38 @@ void print()
   Serial.print("Received br_brake_temperature: ");
   Serial.print(float(br_brake_temperature));
   Serial.print("\n");
-  #ifndef ARDUINO_ARCH_ESP32  // ESP gets sad and doesn't work properly if you serial print too much
-    Serial.print("Received fl_wheel_speed: ");
-    Serial.print(float(test_float));
-    Serial.print("\n");
-    // Serial.print("Received fr_wheel_speed: ");
-    // Serial.print(float(fr_wheel_speed));
-    // Serial.print("\n");
-    // Serial.print("Received bl_wheel_speed: ");
-    // Serial.print(float(bl_wheel_speed));
-    // Serial.print("\n");
-    // Serial.print("Received br_wheel_speed: ");
-    // Serial.print(float(br_wheel_speed));
-    // Serial.print("\n");
-    Serial.print("Received fl_brake_temperature: ");
-    Serial.print(float(fl_brake_temperature));
-    Serial.print("\n");
-    // Serial.print("Received fr_brake_temperature: ");
-    // Serial.print(float(fr_brake_temperature));
-    // Serial.print("\n");
-    // Serial.print("Received bl_brake_temperature: ");
-    // Serial.print(float(bl_brake_temperature));
-    // Serial.print("\n");
-    // Serial.print("Received br_brake_temperature: ");
-    // Serial.print(float(br_brake_temperature));
-    // Serial.print("\n");
-    // Serial.print("Received front_brake_pressure: ");
-    // Serial.print(uint16_t(front_brake_pressure));
-    // Serial.print("\n");
-    // Serial.print("Received rear_brake_pressure: ");
-    // Serial.print(uint16_t(rear_brake_pressure));
-    // Serial.print("\n");
-  #endif
+  // #ifndef ARDUINO_ARCH_ESP32  // ESP gets sad and doesn't work properly if you serial print too much
+  //   Serial.print("Received fl_wheel_speed: ");
+  //   Serial.print(float(test_float));
+  //   Serial.print("\n");
+  //   // Serial.print("Received fr_wheel_speed: ");
+  //   // Serial.print(float(fr_wheel_speed));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received bl_wheel_speed: ");
+  //   // Serial.print(float(bl_wheel_speed));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received br_wheel_speed: ");
+  //   // Serial.print(float(br_wheel_speed));
+  //   // Serial.print("\n");
+  //   Serial.print("Received fl_brake_temperature: ");
+  //   Serial.print(float(fl_brake_temperature));
+  //   Serial.print("\n");
+  //   // Serial.print("Received fr_brake_temperature: ");
+  //   // Serial.print(float(fr_brake_temperature));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received bl_brake_temperature: ");
+  //   // Serial.print(float(bl_brake_temperature));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received br_brake_temperature: ");
+  //   // Serial.print(float(br_brake_temperature));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received front_brake_pressure: ");
+  //   // Serial.print(uint16_t(front_brake_pressure));
+  //   // Serial.print("\n");
+  //   // Serial.print("Received rear_brake_pressure: ");
+  //   // Serial.print(uint16_t(rear_brake_pressure));
+  //   // Serial.print("\n");
+  // #endif
   delay(10);
 }
 
@@ -108,12 +110,21 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("Started");
-  can_bus.Initialize(ICAN::BaudRate::kBaud1M);
+  can_bus1.Initialize(ICAN::BaudRate::kBaud1M);
+  can_bus2.Initialize(ICAN::BaudRate::kBaud1M);
   timer_group.AddTimer(1000, print);
+
+  #ifndef ARDUINO_ARCH_ESP32
+    can_bus1.RegisterRXMessage(fl_wheel_msg); // Temporary workaround
+    can_bus1.RegisterRXMessage(fr_wheel_msg);
+    can_bus2.RegisterRXMessage(bl_wheel_msg);
+    can_bus2.RegisterRXMessage(br_wheel_msg);
+  #endif
 }
 
 void loop()
 {
   timer_group.Tick(millis());
-  can_bus.Tick();
+  can_bus1.Tick();
+  can_bus2.Tick();
 }
